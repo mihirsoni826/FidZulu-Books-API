0542a8e7119b25dbe7e335fab93b83b588e34588
Agrawal, Vaibhav <vaibhav.agrawal199@gmail.com>
2022-11-07 11:16:42 +0530

    location-calc

--- controller/books.controller.js
+++ controller/books.controller.js
@@ -5,8 +5,30 @@ exports.getAllBooks = async (req, res) => {
 
     try {
         console.log(req.query);
+        let tax = 1;
+        let conv = 1;
+        
         let booksRaw = fs.readFileSync(path.resolve("../FidZulu-Books-API/assets/books.json").replace(/\\/g, '/'));
         let booksJSON = JSON.parse(booksRaw);
+
+        if (req.query.location == 'US-NC'){
+          tax = 1.08;
+          conv = 1;
+        }
+        else if(req.query.location == 'IE') {
+          tax = 1.23;
+          conv = 1.35;
+        }
+
+        else if (req.query.location == "IN") {
+          tax = 1.18;
+          conv = 83;
+        }
+        for (let index = 0; index < booksJSON.length; index++) {
+          const element = booksJSON[index];
+          element.price = (element.price * conv * tax).toFixed(2);
+          
+        }
         res.status(200).json(booksJSON);
     }
     catch (error) {
